aceec0680409f5d2e0c69779dd3b21477f5d1eeb
Mior Muhammad Zaki <crynobone@gmail.com>
2020-09-09 18:09:39 +0800

    Update code.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Specification.php
+++ src/Specification.php
@@ -3,10 +3,15 @@
 
 namespace Laravie\Spec;
 
-
 abstract class Specification implements Contracts\Specification
 {
+    /**
+     * Specification's name.
+     */
     abstract public function uid(): string;
 
+    /**
+     * Validate the specification.
+     */
     abstract public function validate(): Contracts\Result;
 }
